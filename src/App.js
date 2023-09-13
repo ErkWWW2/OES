0cea1688fe3455e4a58ac1465ecfc04fab7acc65
Sami Noroozi <68302126+potatohunter69@users.noreply.github.com>
2023-09-13 12:17:56 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import {} from '@mui/material';
+import { } from '@mui/material';
+import MyFormComponent from './components/MyFormComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyFormComponent /> {/* MyFormComponent appears first */}
+     
     </div>
   );
 }
