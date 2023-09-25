20633f66aee3503a0990967c09decc0581a7e427
Isac Gustafsson <92581559+Aaetpio@users.noreply.github.com>
2023-09-25 16:09:46 +0200

    EventView now only dsiplays logUser events
    Made a modification to the return-statement of EventView which adjusts which events are shown based on what user is logged in. IsacG can only see event 1, ErikW can see both events and SamiN can only see event 2.

--- src/components/App/EventLayout/EventView/EventView.js
+++ src/components/App/EventLayout/EventView/EventView.js
@@ -2,23 +2,30 @@ import React, { useState } from "react";
 import { Grid } from '@mui/material';
 import './EventView.css';
 import { useEventContext } from "../../../../server/EventController";
+import { useUserContext } from "../../../../server/UserController";
 
 function EventComponent({ selectedEvent, setSelectedEvent }) {
     const eventController = useEventContext();
+    const userController = useUserContext();
+
+    console.log(userController.logUser);
+    console.log(eventController.eventDetails[1].part);
 
     const events = {};
     eventController.eventDates.forEach(eventDate => {
-        if (!events[eventDate.eventId]) {
+        //if(eventController.eventDetails.part.includes(userController.logUser)){
+            if (!events[eventDate.eventId]) {
             events[eventDate.eventId] = [];
-        }
+            }
 
-        events[eventDate.eventId].push(eventDate);
+            events[eventDate.eventId].push(eventDate); 
+        //}
     })
 
     return (
         <div className="grid">
             <Grid container spacing={5} gridTemplateColumns="repear(12, 3fr)">
-                {eventController.eventDetails.map(event => (
+                {eventController.eventDetails.filter(event => event.part.includes(userController.logUser)).map(event => ( 
                     <Grid item key={event.eventId} xs={12} sm={6} md={4}>
                         <div className="eventBox" onClick={() => setSelectedEvent(event.eventId)}>
                             <div className="eventContent" >
