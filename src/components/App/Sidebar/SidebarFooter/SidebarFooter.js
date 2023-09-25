f651e7b13a22cdfae1567a1cec80205ef22fb199
Sami Noroozi <68302126+potatohunter69@users.noreply.github.com>
2023-09-25 17:18:46 +0200

    User configuration file

--- src/components/App/Sidebar/SidebarFooter/SidebarFooter.js
+++ src/components/App/Sidebar/SidebarFooter/SidebarFooter.js
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom";
 import EventLayout from "../../EventLayout/EventLayout";
-import './SidebarFooter.css';
-import logo from './calendar.png';
+import "./SidebarFooter.css";
+import logo from "./calendar.png";
 
 function SideFooter({ currView }) {
-    const link = (currView === EventLayout)? '/calendar': '/events';
-    const linktext = (currView === EventLayout)? 'the calendar': 'your events';
+  const link = currView === EventLayout ? "/calendar" : "/events";
+  const linktext = currView === EventLayout ? "the calendar" : "your events";
 
-    return(
-        <div className="contentBox">
-            <Link className="viewShiftButton" to={link} >
-                <img src={logo}></img>
-                <div className="tooltiptext">View {linktext} </div>
-            </Link>
-            <Link className="createEventButton" to={'/newEvent'}>
-                <h1>Create a new event</h1>
-                <div className="tooltiptext">Create New Event</div>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="buttonContainer">
+      <Link className="viewShiftButton" to={link}>
+        <img src={logo} alt="Calendar Icon" />
+        <div className="tooltiptext">View {linktext}</div>
+      </Link>
+      <Link className="createEventButton" to={"/newEvent"}>
+        <h1>new event</h1>
+        <div className="tooltiptext">Create New Event</div>
+      </Link>
+      <Link className="createEventButton" to={'/ConfigureProfile'}>
+        <h1>Edit Profile</h1>
+        <div className="tooltiptext">Create New Event</div>
+      </Link>
+
+    </div>
+  );
 }
 
 export default SideFooter;
