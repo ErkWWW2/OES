43c07a25e56a181adfcdff725883a5b4df192977
Erik Wigert <wier21qs@student.ju.se>
2023-10-04 12:16:44 +0200

    Fixed eventView
    The event view is now fully using the server and is independent from the client side eventController.

--- server/controllers/EventController.js
+++ server/controllers/EventController.js
@@ -5,39 +5,15 @@ async function getEventForUser(req, res)
     const userId = req.params.userId;   // Get userId from parameters in the request
 
     // Get events from the mock database
-    const userEvents = mockDetails
-        .filter(event => event.part.includes(parseInt(userId)))
-        .map(event => event.eventId);
+    const userEvents = mockDetails.filter(event => event.part.includes(parseInt(userId)));
 
     console.log(userEvents);            // Log in server
     res.json({ events: userEvents });   // Send response to client
 }
-
-function getEventNameById(req, res) {
-    const eventId = req.params.eventId;
-    const event = mockDetails.find(event => event.eventId === parseInt(eventId)); // Get eventName from mock database
-    if (event) {
-        res.json({ name: event.name });
-    } else {
-        res.status(404).json({ error: 'Event not found' });
-    }
-}
-
-function getEventDescriptionById(req, res) {
-    const eventId = req.params.eventId;
-    const event = mockDetails.find(event => event.eventId === parseInt(eventId)); // Get eventDesc from mock database
-    if (event) {
-      res.json({ description: event.desc });
-    } else {
-      res.status(404).json({ error: 'Event not found' });
-    }
-}
   
 function getEventDatesById(req, res) {
     const eventId = req.params.eventId;
-    const eventDates = mockDates
-        .filter(date => date.eventId === parseInt(eventId))
-        .map(date => ({start: date.start, end: date.end}));
+    const eventDates = mockDates.filter(date => date.eventId === parseInt(eventId));
     res.json(eventDates);
 }
 
--- src/client/pages/w. Sidebar/EventLayout/EventView/EventView.js
+++ src/client/pages/w. Sidebar/EventLayout/EventView/EventView.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Grid } from '@mui/material';
-import { useEventContext } from "../../../../../controllers/EventController";
 import { useUserContext } from "../../../../../controllers/UserController";
 import './EventView.css';
 
 function EventComponent({ setSelectedEvent }) {
     const [userEvents, setUserEvents] = useState([]);
+    const [eventDates, setEventDates] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    const eventController = useEventContext();
     const userController = useUserContext();
 
     const userId = userController.logUser;
@@ -18,65 +18,79 @@ function EventComponent({ setSelectedEvent }) {
     useEffect(() => {
         // GET HTTP request to /api/events/:userId
         axios.get(`/api/events/${userId}`)
-          .then(response => {
-            setUserEvents(response.data.events);
+            .then(response => {
+                setUserEvents(response.data.events);
 
-            const eventDatesPromise = response.data.events.map(eventId => {
-                return axios.get(`/api/event-dates/${eventId}`)
-                    .then(dateResponse => {
-                        return dateResponse.data;
-                    })
-                    .catch(dateError => {
-                        console.error('Error fetching event dates: ', dateError);
-                        return [];
-                    });
-            });
-
-            Promise.all(eventDatesPromise)
-                .then(eventDates => {
-                    console.log("Event Dates for User Events: ", eventDates);
-                })
-                .catch(allDatesError => {
-                    console.error('Error fetching all event dates: ', allDatesError);
+                const eventDatesPromise = response.data.events.map(event => {
+                    return axios.get(`/api/event-dates/${event.eventId}`)
+                        .then(dateResponse => {
+                            return dateResponse.data;
+                        })
+                        .catch(dateError => {
+                            console.error('Error fetching event dates: ', dateError);
+                            return [];
+                        });
                 });
-          })
-          .catch(error => {
-            console.error('Error fetching user events: ', error);
-          });
-      }, []); // Use empty dependency array
 
-    // Get events
-    const events = {};
-    eventController.eventDates.forEach(eventDate => {
-        if (!events[eventDate.eventId]) {
-        events[eventDate.eventId] = [];
-        }
+                Promise.all(eventDatesPromise)
+                    .then(eventDate => {
+                        setEventDates(eventDate);
+                        setIsLoading(false);
+                    })
+                    .catch(allDatesError => {
+                        console.error('Error fetching all event dates: ', allDatesError);
+                        setIsLoading(false);
+                    });
+            })
+            .catch(error => {
+                console.error('Error fetching user events: ', error);
+                setIsLoading(false);
+            });
+    }, []); // Use empty dependency array
 
-        events[eventDate.eventId].push(eventDate); 
-    })
+      // Check the state arrays are correctly updated
+    useEffect(() => {
+        console.log(userEvents);
+        console.log(eventDates);
+    }, [userEvents, eventDates]);
+
+    let events = null;
+    if (!isLoading) {
+        events = userEvents.map(event => {
+            const eventDatesForEvent = eventDates
+                .flatMap(dateArray => dateArray)                                        // Flatten the array of arrays
+                .filter(date => date.eventId === event.eventId)                         // Get only dates for the current event
+                .map(date => ({
+                    start: new Date(date.start), 
+                    end: new Date(date.end), 
+                    votes: date.votes}));  // Get the correct parameters
+    
+            return (
+                <Grid item key={event.eventId} xs={12} sm={6} md={4}>
+                  <div className="eventBox" onClick={() => setSelectedEvent(event.eventId)}>
+                    <div className="eventContent">
+                      <h2>{event.name}</h2>
+                      <hr />
+                      <div>
+                        {eventDatesForEvent.map(item => (
+                          <p key={item}>
+                            <span>{'[' + item.votes + '] '}</span>
+                            <span>{item.start.toLocaleDateString('en-GB', options) + " - " + item.end.toLocaleDateString('en-GB', options)}</span>
+                          </p>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </Grid>
+            );
+        });
+    } 
 
     return (
         <div className="grid">
-            <Grid container spacing={5} gridTemplateColumns="repeat(12, 3fr)">
-                {eventController.eventDetails.filter(event => event.part.includes(userController.logUser)).map(event => ( 
-                    <Grid item key={event.eventId} xs={12} sm={6} md={4}>
-                        <div className="eventBox" onClick={() => setSelectedEvent(event.eventId)}>
-                            <div className="eventContent" >
-                                <h2>{event.name}</h2>
-                                <hr />
-                                <div>
-                                    {events[event.eventId]?.map(item => (
-                                        <p key={item}>
-                                            <span>{'[' + item.votes + '] '}</span>
-                                            <span>{item.start.toLocaleDateString('en-GB', options) + " - " + item.end.toLocaleDateString('en-GB', options)}</span>
-                                        </p>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </Grid> 
-                ))}
-            </Grid>
+          <Grid container spacing={5} gridTemplateColumns="repeat(12, 3fr)">
+            {isLoading ? <p>Loading...</p> : events}
+          </Grid>
         </div>
     );
 }
