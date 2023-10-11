dc577f39e3e044b59c781343bd124be5edac7c39
Sami Noroozi <68302126+potatohunter69@users.noreply.github.com>
2023-10-11 14:34:39 +0200

    fixing mongodb for lgoin

--- server/controllers/serverLoginController.js
+++ server/controllers/serverLoginController.js
@@ -36,7 +36,7 @@ async function login (req, res){
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+   // const passwordMatch = await bcrypt.compare(password, user.password);
 
     //if (!passwordMatch) {
    //   return res.status(401).json({ error: "Invalid credentials" });
