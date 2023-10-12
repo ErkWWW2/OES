1a18be6590b703181142a42e495ff23158bd6b45
Isac Gustafsson <92581559+Aaetpio@users.noreply.github.com>
2023-10-12 11:55:06 +0200

    Added functionality to UserController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -10,4 +10,34 @@ async function getCurrentUserId(req, res)
   }
 }
 
-module.exports = { getCurrentUserId }
+async function getUserById(req, res)
+{
+    try{
+        const user = await User.findOne({id: req.params});
+        res.json({user});
+    }catch(error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+async function updateUser(req, res)
+{
+    try{
+        const filter = {id: req.params.id};
+        const updateOperation = {
+            $set: {
+                username: req.params.username,
+                email: req.params.email,
+                password: req.params.password
+            }
+        };
+
+        await User.updateOne(filter, updateOperation);
+    }catch(error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+module.exports = { getCurrentUserId, getUserById, updateUser };
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const controller = require("../controllers/UserController");
 
 router.get("/getCurrentUserId", controller.getCurrentUserId);
+router.get("/getUserById/:id", controller.getUserById);
+router.put("/updateUser/:id/:username/:email/:password", controller.updateUser);
 
 module.exports = router;
